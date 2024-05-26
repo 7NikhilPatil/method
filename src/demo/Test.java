07ea5143aeb44b0c34d213786a1df00ecfaf74bf
7NikhilPatil <nikhilpatil0710@gmail.com>
2024-05-26 12:55:09 +0530

    initial commit

---
+++ src/demo/Test.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class Test {
+	
+	public void add() {
+
+		int num1 = 10;
+		int num2 = 30;
+
+		System.out.println(num1 + num2);
+	}
+
+	public void sub(int x, int y) {
+		System.out.println(x - y);
+	}
+
+	public void mul(int x, int y) {
+		System.out.println(x * y);
+	}
+
+	public void div(int x, int y) {
+		System.out.println(x / y);
+	}
+
+}
---
+++ src/demo/TestCalculations.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class TestCalculations {
+
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		t.add();
+		t.sub(20, 30);
+		t.mul(15,35);
+		t.div(20,10);
+				
+
+	}
+
+}
